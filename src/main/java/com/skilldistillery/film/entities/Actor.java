f922d8e124f84abb9c6031760d52508b47bfa3b1
tduplantis83 <tduplantis83@gmail.com>
2019-11-01 14:42:05 -0600

    First commit

---
+++ src/main/java/com/skilldistillery/film/entities/Actor.java
@@ -0,0 +1,103 @@
+package com.skilldistillery.film.entities;
+
+import java.util.*;
+
+public class Actor {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private List<Film> films = new ArrayList<>();
+
+	public Actor() {
+
+	}
+
+	public Actor(int id, String firstName, String lastName, List<Film> films) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.films = films;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Film> getFilms() {
+		return films;
+	}
+
+	public void setFilms(List<Film> films) {
+		this.films = films;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Actor other = (Actor) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("First Name: \t");
+		builder.append(getFirstName());
+		builder.append("\nLast Name: \t");
+		builder.append(getLastName());
+		builder.append("\nFilms Starring this Actor: ");
+		for (Film film : films) {
+			builder.append("\n" + film.getTitle());
+		}
+		return builder.toString();
+	}
+
+}
---
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -0,0 +1,303 @@
+package com.skilldistillery.film.entities;
+
+import java.util.*;
+
+public class Film {
+	private int id;
+	private String title;
+	private String description;
+	private Integer releaseYear;
+	private int languageId;
+	private String language;
+	private int rentalDuration;
+	private double rentalRate;
+	private int length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	private String filmCategory;
+	private List<Actor> cast = new ArrayList<>();
+	private Map <String, Integer> conditionCount = new HashMap<>();
+
+	public Film() {
+
+	}
+
+	public Film(int id, String title, String description, Integer releaseYear, int languageId, String language,
+			int rentalDuration, double rentalRate, int length, double replacementCost, String rating,
+			String specialFeatures, List<Actor> cast) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.releaseYear = releaseYear;
+		this.languageId = languageId;
+		this.language = language;
+		this.rentalDuration = rentalDuration;
+		this.rentalRate = rentalRate;
+		this.length = length;
+		this.replacementCost = replacementCost;
+		this.rating = rating;
+		this.specialFeatures = specialFeatures;
+		this.cast = cast;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(Integer year) {
+		this.releaseYear = year;
+	}
+
+	public int getLanguageId() {
+		return languageId;
+	}
+
+	public void setLanguageId(int languageId) {
+		this.languageId = languageId;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+
+	public double getRentalRate() {
+		return rentalRate;
+	}
+
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public double getReplacementCost() {
+		return replacementCost;
+	}
+
+	public void setReplacementCost(double replacementCost) {
+		this.replacementCost = replacementCost;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+
+	public String getFilmCategory() {
+		return filmCategory;
+	}
+
+	public void setFilmCategory(String filmCategory) {
+		this.filmCategory = filmCategory;
+	}
+
+	public List<Actor> getCast() {
+		return cast;
+	}
+
+	public void setCast(List<Actor> cast) {
+		this.cast = cast;
+	}
+
+	public Map<String, Integer> getConditionCount() {
+		return conditionCount;
+	}
+
+	public void setConditionCount(Map<String, Integer> conditionCount) {
+		this.conditionCount = conditionCount;
+	}
+
+	public String allDetails() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("*****ALL " + getTitle() + " Details*****");
+		builder.append("\nFilm ID:\t\t");
+		builder.append(getId());
+		builder.append("\nTitle:\t\t\t");
+		builder.append(getTitle());
+		builder.append("\nDescription:\t\t");
+		builder.append(getDescription());
+		builder.append("\nRelease Year:\t\t");
+		builder.append(getReleaseYear());
+		builder.append("\nLanguage ID:\t\t");
+		builder.append(getLanguageId());
+		builder.append("\nLanguage:\t\t");
+		builder.append(getLanguage());
+		builder.append("\nRental Duration:\t");
+		builder.append(getRentalDuration());
+		builder.append("\nRental Rate:\t\t$");
+		builder.append(getRentalRate());
+		builder.append("\nLength:\t\t\t");
+		builder.append(getLength());
+		builder.append("\nReplacement Cost:\t$");
+		builder.append(getReplacementCost());
+		builder.append("\nRating:\t\t\t");
+		builder.append(getRating());
+		builder.append("\nFilm Category:\t\t");
+		builder.append(getFilmCategory());
+		builder.append("\nSpecial Features:\t");
+		builder.append(getSpecialFeatures());
+		builder.append("\nDescription:\t\t");
+		builder.append(getDescription());
+		builder.append("\n**Actors in this film**");
+		for (Actor actor : cast) {
+			builder.append("\n" + actor.getFirstName() + " " + actor.getLastName());
+		}
+		builder.append("\n**Film Inventory Condition & Counts**");
+		builder.append("\nCondition\t\tNumber In Inventory");
+		for (String condition : conditionCount.keySet()) {
+			String key = condition.toString();
+			Integer num = conditionCount.get(condition);
+			builder.append("\n" + key);
+			builder.append("\t\t\t" + num);
+		}
+		builder.append("\n");
+		return builder.toString();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Title:\t\t\t");
+		builder.append(getTitle());
+		builder.append("\nRelease Year:\t\t");
+		builder.append(getReleaseYear());
+		builder.append("\nRating:\t\t\t");
+		builder.append(getRating());
+		builder.append("\nLanguage:\t\t");
+		builder.append(getLanguage());
+		builder.append("\nDescription:\t\t");
+		builder.append(getDescription());
+		builder.append("\n**Actors in this film**");
+		for (Actor actor : cast) {
+			builder.append("\n" + actor.getFirstName() + " " + actor.getLastName());
+		}
+		builder.append("\n");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + languageId;
+		result = prime * result + length;
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+		result = prime * result + rentalDuration;
+		long temp;
+		temp = Double.doubleToLongBits(rentalRate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(replacementCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((specialFeatures == null) ? 0 : specialFeatures.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((releaseYear == null) ? 0 : releaseYear.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (languageId != other.languageId)
+			return false;
+		if (length != other.length)
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (rentalDuration != other.rentalDuration)
+			return false;
+		if (Double.doubleToLongBits(rentalRate) != Double.doubleToLongBits(other.rentalRate))
+			return false;
+		if (Double.doubleToLongBits(replacementCost) != Double.doubleToLongBits(other.replacementCost))
+			return false;
+		if (specialFeatures == null) {
+			if (other.specialFeatures != null)
+				return false;
+		} else if (!specialFeatures.equals(other.specialFeatures))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (releaseYear == null) {
+			if (other.releaseYear != null)
+				return false;
+		} else if (!releaseYear.equals(other.releaseYear))
+			return false;
+		return true;
+	}
+
+}
